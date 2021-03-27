96b581be5cdca97a7ed0dd6db6b46898ec7aae0d
Yoshihiro Tanaka <contact@cordea.jp>
2021-03-27 17:33:48 +0900

    define new endpoints

--- src/main/kotlin/server.kt
+++ src/main/kotlin/server.kt
@@ -1,5 +1,26 @@
+import csv.csv
 import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.http.*
+import io.ktor.request.*
+import io.ktor.response.*
+import io.ktor.routing.*
 
 fun Application.main() {
+    install(ContentNegotiation) {
+        csv()
+    }
 
+    routing {
+        get("/api") {
+            val csv = listOf(listOf("a", "b", "c"), listOf("d", "e", "f"))
+            call.respond(HttpStatusCode.OK, csv)
+        }
+
+        post("/api") {
+            val csv = call.receive<List<List<String>>>()
+            println(csv)
+            call.respond(HttpStatusCode.OK)
+        }
+    }
 }
