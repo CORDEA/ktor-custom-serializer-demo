62229ff1de95db012057e7233c8e5b984f6fbf7a
Yoshihiro Tanaka <contact@cordea.jp>
2021-03-28 19:24:56 +0900

    update dependencies

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -7,4 +7,7 @@ repositories {
 }
 
 dependencies {
+    implementation("io.ktor:ktor-client-core:1.5.1")
+    implementation("io.ktor:ktor-client-cio:1.5.1")
+    implementation(project(":common"))
 }
