bd86d7eb3bcdaf6176d4ef6d7360d133d49c54df
Yoshihiro Tanaka <contact@cordea.jp>
2021-03-28 19:37:34 +0900

    install feature

--- client/src/main/kotlin/csv/CsvFeature.kt
+++ client/src/main/kotlin/csv/CsvFeature.kt
@@ -2,8 +2,12 @@ package csv
 
 import io.ktor.client.*
 import io.ktor.client.features.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.client.utils.*
 import io.ktor.http.*
 import io.ktor.util.*
+import io.ktor.utils.io.*
 
 class CsvFeature(
     private val serializer: CsvSerializer,
@@ -29,7 +33,41 @@ class CsvFeature(
         override val key: AttributeKey<CsvFeature> = AttributeKey("csv")
 
         override fun install(feature: CsvFeature, scope: HttpClient) {
-            // TODO
+            scope.requestPipeline.intercept(HttpRequestPipeline.Transform) { payload ->
+                feature.acceptContentTypes.forEach { context.accept(it) }
+
+                val contentType = context.contentType() ?: return@intercept
+                if (!feature.canHandle(contentType)) {
+                    return@intercept
+                }
+                context.headers.remove(HttpHeaders.ContentType)
+
+                proceedWith(
+                    when (payload) {
+                        Unit,
+                        is EmptyContent -> EmptyContent
+                        else -> feature.serializer.write(payload, contentType)
+                    }
+                )
+            }
+
+            scope.responsePipeline.intercept(HttpRequestPipeline.Transform) { (info, body) ->
+                if (body !is ByteReadChannel) {
+                    return@intercept
+                }
+
+                val contentType = context.response.contentType() ?: return@intercept
+                if (!feature.canHandle(contentType)) {
+                    return@intercept
+                }
+
+                proceedWith(
+                    HttpResponseContainer(
+                        info,
+                        feature.serializer.read(info, body.readRemaining())
+                    )
+                )
+            }
         }
 
         override fun prepare(block: Config.() -> Unit): CsvFeature {
